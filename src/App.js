8da64fde69953e2e0cc3b487597d18faf200a21a
vishnusj <vishnusj12@gmail.com>
2022-02-23 19:17:27 +0530

    state&props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import MyComponent from './components/MyComponent';
+import MyForm from './components/Form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      people: ['robert', 'jack', 'prison mike', 'oscar', 'jim', 'pamela'],
+    };
+    this.countUp = this.countUp.bind(this);
+    this.AddPeople = this.AddPeople.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({
+      count: this.state.people.length,
+    });
+  }
+
+  countUp() {
+    this.setState(
+      {
+        count: this.state.count + 1,
+      },
+      this.getSnapshotBeforeUpdate
+    );
+  }
+
+  AddPeople(e) {
+    e.preventDefault();
+    this.countUp();
+    console.log('YAY');
+  }
+
+  render() {
+    return (
+      <div>
+        <MyComponent name={randomPerson(this.state.people)} />
+        <p>Number of peoples : {this.state.count}</p>
+        <MyForm AddPeople={this.AddPeople} />
+      </div>
+    );
+  }
+}
+
+function randomPerson(arr) {
+  const person = arr[Math.floor(Math.random() * arr.length)];
+  return person;
 }
 
 export default App;
