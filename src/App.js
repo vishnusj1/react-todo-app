804bff810e656ad1a421208e7dbf40d4e0ae6589
vishnusj <vishnusj12@gmail.com>
2022-03-01 19:20:28 +0530

    react-todo-app

--- src/App.js
+++ src/App.js
@@ -1,1 +1,97 @@
 import React from 'react';
+import Overview from './components/Overview';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: this.props.tasks,
+      title: '',
+      count: this.props.tasks.length,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.addTask = this.addTask.bind(this);
+    this.handleCompleteTask = this.handleCompleteTask.bind(this);
+    this.deleteTask = this.deleteTask.bind(this);
+    this.editTask = this.editTask.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      title: e.target.value,
+    });
+  }
+  addTask(e) {
+    e.preventDefault();
+    if (this.state.title === '') return;
+    const lastTask = this.state.tasks[this.state.tasks.length - 1];
+    const lastTaskId = lastTask ? lastTask.id : 0;
+    const newTask = {
+      userId: 1,
+      id: lastTaskId + 1,
+      title: this.state.title,
+      completed: false,
+    };
+
+    this.setState((prevState) => ({
+      tasks: this.state.tasks.concat(newTask),
+      title: '',
+      count: prevState.count + 1,
+    }));
+  }
+
+  deleteTask(id) {
+    console.log(id);
+    const tasks = this.state.tasks.filter((obj) => obj.id !== id);
+    this.setState((prevState) => ({
+      tasks: tasks,
+      count: prevState.count - 1,
+    }));
+  }
+
+  handleCompleteTask(task) {
+    const arr = [...this.state.tasks];
+    const index = arr.findIndex((obj) => obj.id === task.id);
+    const obj = { ...arr[index], completed: !task.completed };
+    arr[index] = obj;
+    this.setState(
+      {
+        tasks: arr,
+      },
+      () => console.log(this.state.tasks)
+    );
+  }
+
+  editTask(task, text) {
+    const arr = [...this.state.tasks];
+    const index = arr.findIndex((obj) => obj.id === task.id);
+    const obj = { ...arr[index], title: text };
+    arr[index] = obj;
+    this.setState(
+      {
+        tasks: arr,
+      },
+      () => console.log(this.state.tasks[index])
+    );
+  }
+  render() {
+    const { tasks, title, count } = this.state;
+    return (
+      <div className="container">
+        <form onSubmit={this.addTask}>
+          <input type="text" placeholder="Add task" onChange={this.handleChange} value={title} />
+          <button type="submit">Add</button>
+        </form>
+        <Overview
+          tasks={tasks}
+          count={count}
+          handleCompleteTask={this.handleCompleteTask}
+          deleteTask={this.deleteTask}
+          editTask={this.editTask}
+        />
+      </div>
+    );
+  }
+}
+export default App;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,72 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const TASKS = [
+  {
+    userId: 1,
+    id: 1,
+    title: 'delectus aut autem',
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 2,
+    title: 'quis ut nam facilis et officia qui',
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 3,
+    title: 'fugiat veniam minus',
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 4,
+    title: 'et porro tempora',
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 5,
+    title: 'laboriosam mollitia et enim quasi adipisci quia provident illum',
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 6,
+    title: 'qui ullam ratione quibusdam voluptatem quia omnis',
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 7,
+    title: 'illo expedita consequatur quia in',
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 8,
+    title: 'quo adipisci enim quam ut ab',
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 9,
+    title: 'molestiae perspiciatis ipsa',
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 10,
+    title: 'illo est ratione doloremque quia maiores aut',
+    completed: true,
+  },
+];
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App tasks={TASKS} />
   </React.StrictMode>,
   document.getElementById('root')
 );
